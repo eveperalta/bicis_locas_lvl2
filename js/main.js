d0a1f798f43655c7b38ddb604647b7d44f96e794
EvePeralta <eperalta.cm@gmail.com>
2016-12-17 16:45:18 -0300

    cambie el codigo en el submit

--- js/main.js
+++ js/main.js
@@ -1,59 +1,88 @@
 function validateForm(){
-	validarCampoTexto($("#name"));
-	validarCampoTexto($("#lastname"));
-	ValidaEmail($("#input-email"));
-	validaPwd($("#input-password"));
-	validarChkbx($(".checkbox"));
-	validarSelect($(".form-control"));
+
+	if(validarCampoTexto($("#name")) == false){
+		return false
+	}
+	if(validarCampoTexto($("#lastname"))== false){
+		return false
+	}
+	if(ValidaEmail($("#input-email"))== false){
+		return false
+	}
+	if(validaPwd($("#input-password"))== false){
+		return false
+	}
+	if(validarChkbx($(".checkbox"))== false){
+		return false
+	}
+	if(validarSelect($(".form-control"))== false){
+		return false
+	}
+	return true
 };
 //validate name, lastname
 function validarCampoTexto(input){
 	var mensaje= "";
+	var bool = true;
+
 	if($(input).val() == ""){
 		mensaje += "Campo Obligatorio "
+		bool = false
 	}else{
 		if($(input).val().length >30){
 			mensaje += "Debe tener menos de 30 caracteres "
+			bool = false
 		}
 		if($(input).val().charAt(0).toUpperCase() != $(input).val().charAt(0)){
 			mensaje += "Primera letra debe ser mayúscula "
+			bool = false
 		}
 		if(!($(input).val().match(/^[a-zA-Z]+$/))){
 			mensaje += "Sólo debe contener letras "
+			bool = false
 		}
 	}
 	$(input).siblings().filter("span").remove();
 	// colocar este span, ssi es != ""
 	mostrarMsg(mensaje, input);
+	return bool
 };
 
 //validate email
 function ValidaEmail(input) {
 	var mensaje ="";
+	var bool=true;
 	if($(input).val() == ""){
 		mensaje += "campo obligatorio"
+		bool = false
 	}else{
 		if(!($(input).val().match(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/))){
 			mensaje += "Debe ser un correo válido"
+			bool = false
 		}
 	}
 	$(input).siblings().filter("span").remove();
 	// colocar este span, ssi es != ""
 	mostrarMsg(mensaje, input);
+	return bool
 };
 
 function validaPwd(input){
 	var mensaje = "";
+	var bool=true;
 	if($(input).val() == ""){
 		mensaje += "campo obligatorio"
+		bool = false
 	}else if(!($(input).val().match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/))){
 		mensaje += "La contraseña debe contener 8 caracteres, al menos 1 numero, 1 mayúscula, 1 minúscula"
+		bool = false
 	}
 	// limpiamos los otros span
 	$(input).siblings().filter("span").remove();
 
 	// colocar este span, ssi es != ""
 	mostrarMsg(mensaje, input);
+	return bool
 };
 
 
@@ -66,28 +95,42 @@ function mostrarMsg(mensaje, input){
 	$(input).parent().append(span_nombre);
 };
 //funcion para el checkbox
+
 function validarChkbx(input){
+	var bool=true;
 	if($(':checkbox').is(":checked")){
-		return
+		return bool
 	}else{
 		var span_nombre = $("<span class='error'>" + "Este botón es obligatorio" + "</span>");
 		$(':checkbox').parent().append(span_nombre);
-
+		bool = false
 	}
+	return bool
 };
         
 //funcion para el select
 function validarSelect(input){
+	var bool=true;
 	if($("select").val()== 0){
 		 var span_nombre = $("<span class='error2'>" + "Selecciona un tipo de Bici" + "</span>");
 		$('select').parent().append(span_nombre);
+		bool = false
 	}
+	return bool
 };
 
-/*funcion para esconder el formulario
-$(".form-signup").on("submit",function onClickClose(){
-    $(".form-signup").hide();
-});*/
+//funcion para esconder el formulario
+$(".form-signup").on("submit", function(e){
+	e.preventDefault()
+	if(validateForm() == true){
+		$(".form-signup").hide()
+		$("#planetas").removeClass("hidden")
+	}else{
+		console.log("formulario")
+	}
+
+ //  $(".form-signup").hide();
+});
 
 // ajax!
 $.ajax({
